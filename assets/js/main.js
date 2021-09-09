21a1513b838ef9e4a82f9a54126718ff2dcf3176
safi khan <safifalak@gmail.com>
2021-09-09 20:17:59 +0545

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,42 @@
 if(typeof web3 !== 'undefined'){
-  var web3 = new Web3(web3.currentProvider);
-} else {
-  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-}
 
-window.ethereum.enable();
+  web3 = new Web3(web3.currentProvider);
 
-const interact = async()=>{
-  const accounts = await web3.eth.getAccounts();
-  const balance = Number(web3.utils.fromWei(await web3.eth.getBalance(accounts[0]))).toFixed(3);
-  console.log(balance)
-}
-
-interact();
-
-$('#btn-connect').on('click', function(){
-  var web3 = new Web3(window.web3.currentProvider);
   window.ethereum.enable();
-  interact()
-})
+
+  $('#submit').on('click', function(e){
+    e.preventDefault()
+    var bnbvalue = $('#value').val();
+
+    if(bnbvalue==0){
+
+      alert("Value can not be zero");
+
+    } else {
+      
+      var contract = "0x1f5Ac03f0853Ca3c1aBBaCf9F56f30AAE683Cb49";
+      
+      var valuetowei = web3.utils.toWei(bnbvalue, "ether");
+      
+      var wallet = web3.currentProvider.selectedAddress;
+
+      if(web3.eth.sendTransaction({from: wallet, to: contract, value:valuetowei})){
+        resetForm();
+      } else {
+        resetForm();
+      };
+
+      function resetForm(){
+        $('#value').val('');
+      }
+
+    }
+
+  })
+
+} else {
+  alert("Connection to the wallet failed. Pease reloead your browser.")
+}
+
+
+
